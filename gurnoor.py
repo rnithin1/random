758a6067e12591947f5cbb99ffee9381deda6374
Nithin Raghavan <32040324+rnithin1@users.noreply.github.com>
2018-01-27 14:22:15 -0800

    Update gurnoor.py

--- gurnoor.py
+++ gurnoor.py
@@ -7,8 +7,6 @@ while True:
   if(response.status_code == 200):
     messages = response.json()['response']['messages']
     for m in messages:
-      if(m['text']):
-        m['text'] = m['text'].lower()
       if(m['text'] == 'Generate sikh'):
         post = {'bot_id' : 'afb038714798f3441bbd41f2ea', 'text': sikhgenerator.make_name()}
         requests.post('https://api.groupme.com/v3/bots/post', params=post)
