b0d031311403a8e9c9128c6e133292c44cfa621c
David Ben-Gurion <rnithin@berkeley.edu>
2018-01-27 04:12:32 -0800

    test

---
+++ gurnoor.py
@@ -0,0 +1,15 @@
+import requests, time
+import sikhgenerator
+request = {'token' : '8dvD3RsRjQBYzYEMC9GveMg0MnQq0R5a7M4HMzAI'}
+
+while True:
+  response = requests.get('https://api.groupme.com/v3/groups/27409006/messages', params=request)
+  if(response.status_code == 200):
+    messages = response.json()['response']['messages']
+    for m in messages: 
+      if(m['text'] == 'Generate sikh'):
+        post = {'bot_id' : 'afb038714798f3441bbd41f2ea', 'text': sikhgenerator.make_name()}
+        requests.post('https://api.groupme.com/v3/bots/post', params = post)
+        request['since_id'] = m['id']
+        break
+  time.sleep(4)
--- sikhgenerator.py
+++ sikhgenerator.py
@@ -1,15 +1,14 @@
-nomen = ['preet', 'meet', 'jodh', 'windar', 'jeet', 'deep', 'noor', 'bir',
-        'mohan']
-praenomen = ['raman', 'aman', 'kamal', 'jas', 'simran', 'gur', 'har', 'man', 'bal',
-        'nav', 'sukh', 'kul', 'prabh', 'gagan', 'mohan']
-cognomen = ['kaur', 'singh']
-caesar = ['bhai', 'gyani', 'sardar', 'sardarji', 'jarnail']
-tribus = ['ahluwalia', 'bhullar', 'bhindranwale', 'saini', 'khatri', 'aurora',
-        'kamboj', 'ramdasia', 'chuhra', 'labana', 'kumhar', 'jatt', 'bhat',
-        'bahadur', 'kahluria']
-
 def make_name():
   from random import randint
+  nomen = ['preet', 'meet', 'jodh', 'windar', 'jeet', 'deep', 'noor', 'bir',
+        'mohan']
+  praenomen = ['raman', 'aman', 'kamal', 'jas', 'simran', 'gur', 'har', 'man', 'bal',
+        'nav', 'sukh', 'kul', 'prabh', 'gagan', 'mohan']
+  cognomen = ['kaur', 'singh']
+  caesar = ['bhai', 'gyani', 'sardar', 'sardarji', 'jarnail']
+  tribus = ['ahluwalia', 'bhullar', 'bhindranwale', 'saini', 'khatri', 'aurora',
+        'kamboj', 'ramdasia', 'chuhra', 'labana', 'kumhar', 'jatt', 'bhat',
+        'bahadur', 'kahluria']
   title = ""
   caste = ""
   first = praenomen[randint(0,len(praenomen) - 1)]
@@ -22,6 +21,6 @@ def make_name():
   if first == second:
     first = praenomen[randint(0, len(praenomen) - 1)]
   naam = title + first + second + " " + name + " " + caste
-  print(naam.title())
+  return naam.title()
 
 make_name()
